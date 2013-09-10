8c9fc4a5e357b90d0b6c339d02401c362fa675c7
luis Nunez <lnunez@c3isecurity.com>
2013-09-10 14:55:51 -0400

    initial commit for powershell remediation scripts

---
+++ PowerShell/poweshell_notes.ps1
@@ -0,0 +1,22 @@
+﻿<# Basic Remediation PowerShell script #>
+<# Intent to address Windows 7 STIG settings #>
+
+<# Read a registry key #>
+<# Get-ChildItem -path hkcu:\ #>
+
+<# Create new registry key #>
+<# New-Item -Path HKCU:\Software\_DeleteMe #> 
+
+<# Delete registry key #>
+<# Remove-Item -path HKCU:\Software_DeleteMe -Verbose #>
+
+<# Create new registry key value #>
+<# New-ItemProperty -Path HKCU:\Software\_DeleteME -Name PowerShellTest -PropertyType String -Value PS1 -Verbose #>
+
+<# Update (change) registry key name #>
+<# Rename-ItemProperty -Path HKCU:\Software\_DeleteMe -Name PowerShellTest -NewName PowerShellTest1 -Verbose #>
+
+<# Update (change) registry key vaule #>
+Set-ItemProperty -Path HKCU:\Software\_DeleteMe -Name PowerShellTest1 -Value PS3  -Verbose
+
+
