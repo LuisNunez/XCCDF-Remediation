74179744c3a02edd17c90f44e964641a285e036e
luis Nunez <lnunez@c3isecurity.com>
2013-09-11 15:11:16 -0400

    adding win7stig_F-29830r1_fix.ps1 Remediation script.

---
+++ PowerShell/win7stig_F-29830r1_fix.ps1
@@ -0,0 +1,43 @@
+﻿<# PowerShell script to remediate #>
+
+<# Based on DISA Windows 7 STIG Benchmark Version 1 Release 6 #>
+<# Maximum Log Size - Application #>
+<# Rule ID SV-36021r1_rule #>
+<# CCE-9603-2 #>
+<# fix id F-29830r1_fix #>
+<#  HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application MaxSize value #>
+<# regdword #>
+<# value 32768 #>
+
+
+<#
+New-Item -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\EventLog\
+New-Item -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application
+#>
+
+Write-Output "Starting Remediation"
+$a = Test-Path -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\EventLog\
+$b = Test-Path -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application
+
+if ( $a -eq "True")
+    {
+        Write-Host "key exists"
+    }
+    else
+    {
+        Write-Host "key does not exist"
+        Write-Host "Creating key"
+        New-Item -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\EventLog
+    }
+
+if ($b -eq "True")
+    {
+        Write-Host "key exists"
+    }
+    else
+    {
+        Write-Host "key does not exist"
+        Write-Host "Creating key"
+        New-Item -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application
+    }
+
